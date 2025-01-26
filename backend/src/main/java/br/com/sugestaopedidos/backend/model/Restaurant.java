2d4b7bae1cdd6df3f333aa4a6d60bb7e67c66013
Otavio Marques <marquesotavio251204@gmail.com>
2025-01-26 17:49:00 -0300

    crud restaurantes

---
+++ backend/src/main/java/br/com/sugestaopedidos/backend/model/Restaurant.java
@@ -0,0 +1,40 @@
+package br.com.sugestaopedidos.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "restaurants")
+@Data
+@AllArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String description;
+    private String address;
+    private String phone;
+    private String cnpj;
+    private String ie;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Restaurant() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ backend/src/main/java/br/com/sugestaopedidos/backend/service/RestaurantService.java
@@ -0,0 +1,59 @@
+package br.com.sugestaopedidos.backend.service;
+
+import br.com.sugestaopedidos.backend.dto.RestaurantRequestDto;
+import br.com.sugestaopedidos.backend.dto.RestaurantResponseDto;
+import br.com.sugestaopedidos.backend.exception.resource.ResourceNotFoundException;
+import br.com.sugestaopedidos.backend.mapper.RestaurantMapper;
+import br.com.sugestaopedidos.backend.model.Restaurant;
+import br.com.sugestaopedidos.backend.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantMapper restaurantMapper;
+
+    public Set<RestaurantResponseDto> findAllRestaurants() {
+        return restaurantRepository.findAll().stream()
+                .map(restaurantMapper::toDto)
+                .collect(Collectors.toSet());
+    }
+
+    public RestaurantResponseDto findByIdRestaurants(String id) {
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> getResourceNotFoundException(id));
+
+        return restaurantMapper.toDto(restaurant);
+    }
+
+    public RestaurantResponseDto createRestaurant(RestaurantRequestDto restaurantRequestDto) {
+        Restaurant restaurant = restaurantMapper.toEntity(restaurantRequestDto);
+        restaurant = restaurantRepository.save(restaurant);
+        return restaurantMapper.toDto(restaurant);
+    }
+
+    public void updateRestaurant(String id, RestaurantRequestDto restaurantRequestDto) {
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> getResourceNotFoundException(id));
+
+        restaurantMapper.updateEntityFromRequest(restaurant, restaurantRequestDto);
+
+        restaurantRepository.save(restaurant);
+    }
+
+    public void delete(String id) {
+        if(!restaurantRepository.existsById(id)) throw getResourceNotFoundException(id);
+
+        restaurantRepository.deleteById(id);
+    }
+
+    private static ResourceNotFoundException getResourceNotFoundException(String id) {
+        return new ResourceNotFoundException("Restaurant not found with id: " + id);
+    }
+}
