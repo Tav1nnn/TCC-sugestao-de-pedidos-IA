03d135d04092261005dff962b05cc5b1492985df
Otavio Marques <marquesotavio251204@gmail.com>
2025-03-07 21:34:09 -0300

    cors

--- backend/src/main/java/br/com/sugestaopedidos/backend/controller/OpenAiController.java
+++ backend/src/main/java/br/com/sugestaopedidos/backend/controller/OpenAiController.java
@@ -4,16 +4,14 @@ import br.com.sugestaopedidos.backend.dto.ChatDto;
 import br.com.sugestaopedidos.backend.service.ChatService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/chat")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000/")
 public class OpenAiController {
     private final ChatService chatService;
 
