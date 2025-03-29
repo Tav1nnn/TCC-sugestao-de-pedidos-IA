6ec06826d19c0c3c0818cd02c6561eb37f1c252c
GuAparecido <gustavorcdaparecido@gmail.com>
2025-03-29 17:13:43 -0300

    "Atualizando o front web"

--- frontend/sugereai/src/pages/Home.js
+++ frontend/sugereai/src/pages/Home.js
@@ -6,57 +6,7 @@ import {
 } from "@chakra-ui/react";
 import { FaRobot } from "react-icons/fa";
 import axios from 'axios';
-
-/*const restaurants = [
-  {
-    id: 1,
-    name: 'Restaurante A',
-    description: 'Descrição do Restaurante A',
-    image: 'https://marketplace.canva.com/EAFuRCowrBE/1/0/1600w/canva-logotipo-para-restaurante-moderno-laranja-marrom-M2uDDAK9eXw.jpg'
-  },
-  {
-    id: 2,
-    name: 'Restaurante B',
-    description: 'Descrição do Restaurante B',
-    image: 'https://static.vecteezy.com/ti/vetor-gratis/p1/9291628-restaurante-logo-design-vetor.jpg'
-  },
-  {
-    id: 3,
-    name: 'Restaurante C',
-    description: 'Descrição do Restaurante C',
-    image: 'https://img.freepik.com/vetores-gratis/logotipo-do-restaurante-retro_23-2148490227.jpg'
-  },
-  {
-    id: 4,
-    name: 'Restaurante D',
-    description: 'Descrição do Restaurante D',
-    image: 'https://static.vecteezy.com/ti/vetor-gratis/p1/10411845-restaurante-logo-design-modelo-gratis-vetor.jpg'
-  },
-  {
-    id: 5,
-    name: 'Restaurante E',
-    description: 'Descrição do Restaurante E',
-    image: 'https://img.freepik.com/vetores-premium/logotipo-do-restaurante-retro_23-2148474404.jpg'
-  },
-  {
-    id: 6,
-    name: 'Restaurante F',
-    description: 'Descrição do Restaurante F',
-    image: 'https://www.creativefabrica.com/wp-content/uploads/2019/08/Restaurant-Logo-by-Koko-Store-580x386.jpg'
-  },
-  {
-    id: 7,
-    name: 'Restaurante G',
-    description: 'Descrição do Restaurante G',
-    image: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/logotipo-restaurant-design-template-208dd1149913cdd7974bb7d832a59dc4_screen.jpg?ts=1625803921'
-  },
-  {
-    id: 8,
-    name: 'Restaurante H',
-    description: 'Descrição do Restaurante H',
-    image: 'https://images.vexels.com/content/172815/preview/restaurant-logo-design-5460fc.png'
-  }
-];*/
+import logo from '../images/Logo preta escrita.png';
 
 const Home = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -82,7 +32,8 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home-header">
-        <h1>Restaurantes</h1>
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>RESTAURANTES</h1>
       </div>
       <div className="home-image">
         <div className='sugere-ai'>
@@ -104,7 +55,6 @@ const Home = () => {
           <a href='http://localhost:3000/chat' rel='noopener noreferrer'> {/* Adicionar validação no Button */}
             <Button className='btn-ia'>
               <FaRobot className='icon-ia' />
-              {/*<PiOpenAiLogoBold className='icon-ia'/> */}
             </Button>
           </a>
         </div>
@@ -112,9 +62,5 @@ const Home = () => {
     </div>
   );
 };
-
-/* Nunca foi tão fácil saber o que pedir, aqui você não fica na dúvida 
-    Placeholder "Diga ao chat o que você deseja comer".
-*/
 
 export default Home;
